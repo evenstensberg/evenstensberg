46b93777a733b1fa5c102992f96eea028fbd0821
Even Stensberg <evenstensberg@gmail.com>
2019-11-02 09:43:48 +0100

    chore: wip

--- app.js
+++ app.js
@@ -1,44 +1,86 @@
-const express = require('express');
-const path = require('path')
-const app = express();
-const socketIO = require("socket.io");
-const http = require('http');
 const favicon = require('serve-favicon');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('cookie-session');
+const http = require('http');
+const https = require('https');
+const cors = require("cors")
+const app = express();
+const config = require('./api/config.js');
 
-app.use(express.static('public'))
+app.use(cors());
+app.options('*', cors());
+
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(__dirname + '/favico.ico'));
+
+/* app.use(
+	session({
+		secret: config.secret,
+		signed: true,
+	}),
+); */
+
 app.get('*', function(req, res) {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')})
 });
 
-const server = http.createServer(app);
+app.use('/', require('./api/routes')(config));
 
-const io = socketIO(server);
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
 
-io.on("connection", socket => {
-    console.log("New client connected" + socket.id);
-
-    /* socket.on("yolo", () => {
-        console.log("Hi from server")
-        try {
-            const {spawn} = require("child_process"); 
-            const twProc = spawn('python3',["../twitter-fun/index.py"]); 
-            const spotifyProc = spawn('python3',["../spotify-api-benchmark/index.py"] );
-
-            twProc.stdout.on('data', function(data) {
-                console.log(data);
-            })
-            spotifyProc.stdout.on('data', function(data) { 
-                console.log(data);
-            }) 
-        } catch(err) {
-            console.log(err)
-        }); */
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
+
+app.use(function(req, res) {
+	res.status(404).send('Not Found');
+});
+
+// Basic error handler
+app.use(function(err, req, res, next) {
+	console.error(err);
+
+	// Send response if exists, if not send a custom message
+	res.status(500).send(err.response || 'Server failed!');
+});
+
+const httpServer = http.createServer(app);
+httpServer.listen(3000);
+
+const server = https.createServer({}, app);
 
 // Start the server
 const PORT = process.env.PORT || 8080;
+
 server.listen(PORT, () => {
-  console.log(`App listening on port ${PORT} 🥰`);
-  console.log('Press Ctrl+C to quit.');
+    console.log(`App listening on port ${PORT} 🥰`);
+    console.log('Press Ctrl+C to quit.');
 });
+
